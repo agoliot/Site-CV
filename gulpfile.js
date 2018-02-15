077108d83f3364c8b681176289d8fc38dd31e8b3
agoliot <goliot.anthony@gmail.com>
2018-02-15 17:49:03 +0100

    changement repertoire ajout browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,30 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
-});
+gulp.task('default', ['serve']);
 
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./"
+        server: "./app"
     });
 
-  	gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch("**/*.html").on('change', browserSync.reload);
+  	gulp.watch('./app/sass/**/*.scss', ['sass']);
+    gulp.watch("./app/**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve']);
+ 
+gulp.task('sass', function () {
+  return gulp.src('./app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
