80a9bd9da858f095bec9ceb7357e3d996029e037
Anthony Goliot <goliot.anthony@gmail.com>
2018-02-18 23:28:42 +0100

    modification tout le contenu

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,50 @@
-$(".menu-access").hover(function() {
-    $(".menu").addClass("menu-open");
+$(window).on('scroll', function() {
+    stickyNavigation();
 });
 
+function stickyNavigation() {
+    var lnStickyNavigation = $('.scroll-down').offset().top + 20;
+    if ($(window).scrollTop() > lnStickyNavigation) {
+        $('body').addClass('fixed');
+    } else {
+        $('body').removeClass('fixed');
+    }
+}
 
-$(".menu-close").click(function() {
-    $(".menu").removeClass("menu-open");
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navigation a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navigation ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+    });
+}
